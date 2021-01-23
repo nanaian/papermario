dc342c0218354c360f7cbe5651c1423aa581bd15
Alex Bates <hi@imalex.xyz>
2021-01-23 10:25:25 +0000

    kkj_28

--- src/world/area_kkj/kkj_28/kkj_28.c
+++ src/world/area_kkj/kkj_28/kkj_28.c
@@ -3,9 +3,6 @@
 Script N(script_ExitSingleDoor_80240050);
 Script N(script_EnterSingleDoor_802400F4);
 Script N(main);
-s32 pad_0001FC[1];
-const char D_80240200_B150A0[8]; // "kkj_10"
-s32 pad_000208[2];
 
 EntryList N(entryList) = {
     { 325.0f, 0.0f, -30.0f, 270.0f },
@@ -15,7 +12,6 @@ MapConfig N(header) = {
     .main = N(main),
     .entryList = N(entryList),
     .entryCount = ENTRY_COUNT(N(entryList)),
-    .tattle = 0x0,
 };
 
 Script N(script_ExitSingleDoor_80240050) = SCRIPT({
@@ -27,7 +23,7 @@ Script N(script_ExitSingleDoor_80240050) = SCRIPT({
     SI_VAR(3) = -1;
     spawn ExitSingleDoor;
     sleep 17;
-    GotoMap(D_80240200_B150A0, 2);
+    GotoMap("kkj_10", 2);
     sleep 100;
 });
 
@@ -48,14 +44,3 @@ Script N(main) = SCRIPT({
     bind N(script_ExitSingleDoor_80240050) to TriggerFlag_WALL_INTERACT 2;
     spawn N(script_EnterSingleDoor_802400F4);
 });
-
-s32 pad_0001FC[] = {
-    0x00000000,
-};
-
-// rodata: D_80240200_B150A0
-
-s32 pad_000208[] = {
-    0x00000000, 0x00000000,
-};
-
