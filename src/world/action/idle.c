837f21d41d83bb7126b2091a6682257f10c85d25
Alex Bates <hi@imalex.xyz>
2021-02-10 16:20:49 +0000

    name world/action/idle segment

---
+++ src/world/action/idle.c
@@ -0,0 +1,7 @@
+#include "common.h"
+
+#define NAMESPACE world_action_idle
+
+INCLUDE_ASM(s32, "world/action/idle", world_action_idle_update);
+
+INCLUDE_ASM(s32, "world/action/idle", func_802B61E4_E23444);
